aba837bef7b35bcb7e84a14469617f8b186f4460
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2019-12-08 13:09:32 -0200

    Colocado startClass no pom.xml

--- src/main/java/com/baeldung/springbootbaeldung/SpringbootBaeldungApplication.java
+++ src/main/java/com/baeldung/springbootbaeldung/SpringbootBaeldungApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBaeldungApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBaeldungApplication.class, args);
 	}
-
 }
