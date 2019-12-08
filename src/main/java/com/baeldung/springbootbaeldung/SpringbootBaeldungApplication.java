4808d1390c1f415d163ec36f3582b1e1466d09f2
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2019-12-08 13:01:54 -0200

    Commit inicial

---
+++ src/main/java/com/baeldung/springbootbaeldung/SpringbootBaeldungApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springbootbaeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBaeldungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBaeldungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baeldung/springbootbaeldung/SpringbootBaeldungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.springbootbaeldung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBaeldungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
