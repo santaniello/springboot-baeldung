c96f0b078cdf38d1d94dddb93b6f8a078ea43bae
Felipe Santaniello <felipe_sbc_sp@hotmail.com>
2020-01-14 21:49:12 -0200

    Implementado validação na classe USER

--- application/src/main/java/com/baeldung/springboot/dtos/UserDTO.java
+++ application/src/main/java/com/baeldung/springboot/dtos/UserDTO.java
@@ -3,8 +3,13 @@ package com.baeldung.springboot.dtos;
 import com.baeldung.springboot.models.Resource;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 public class UserDTO implements Resource {
     private Long id;
+    @NotNull
+    @Size(min = 2, max = 30)
     private String name;
 }
--- rest/src/main/java/com/baeldung/springboot/apis/UserAPI.java
+++ rest/src/main/java/com/baeldung/springboot/apis/UserAPI.java
@@ -5,6 +5,8 @@ import com.baeldung.springboot.dtos.UserDTO;
 import com.baeldung.springboot.services.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -19,7 +21,7 @@ public class UserAPI extends AbstractController<UserDTO> {
     }
 
     @PostMapping
-    public void save(@RequestBody UserDTO dto){
+    public void save(@RequestBody @Valid  UserDTO dto){
        saveInternall(dto);
     }
 
